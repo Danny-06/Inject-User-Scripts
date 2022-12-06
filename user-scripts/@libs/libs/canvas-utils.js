49e0fc460364512e096e56292948c212d7fc8f55
“Daniel” <daniel.fabian13x@gmail.com>
2022-12-06 13:09:17 +0100

    Update CanvasUtils

--- user-scripts/@libs/libs/canvas-utils.js
+++ user-scripts/@libs/libs/canvas-utils.js
@@ -1,4 +1,4 @@
-export function drawMediaInCanvas(media, options = {}) {
+export function getCanvasFromMedia(media, options = {}) {
   const {width, height} = options
 
   const canvas = document.createElement('canvas')
@@ -12,47 +12,58 @@ export function drawMediaInCanvas(media, options = {}) {
   return canvas
 }
 
-export function getMediaAsDataURL(media, options = {}) {
-  const {type, quality} = options
+export function getDataURLFromMedia(media, options = {}, canvasOptions = {}) {
+  const {type, quality} = canvasOptions
 
-  const canvas = drawMediaInCanvas(media, options)
+  const canvas = getCanvasFromMedia(media, options)
 
   return canvas.toDataURL(type, quality)
 }
 
-export function getMediaAsBlob(media, options) {
-  const canvas = drawMediaInCanvas(media, options)
+export async function getBlobFromMedia(media, options = {}, canvasOptions = {}) {
+  const canvas = getCanvasFromMedia(media, options)
+
+  const blob = await getBlobFromCanvas(canvas, canvasOptions)
+
+  return blob
+}
+
+export function getBlobFromCanvas(canvas, canvasOptions = {}) {
+  const {type, quality} = canvasOptions
 
   return new Promise((resolve, reject) => {
     try {
-      canvas.toBlob(blob => resolve(blob))
+      canvas.toBlob(blob => resolve(blob), type, quality)
     } catch (error) {
       reject(error)
     }
   })
 }
 
-export function getMediaAsImageData(media, options) {
-  const canvas = drawMediaInCanvas(media, options)
+
+export function getImageDataFromMedia(media, options) {
+  const canvas = getCanvasFromMedia(media, options)
   const ctx = canvas.getContext('2d')
 
   return ctx.getImageData(0, 0, canvas.width, canvas.height)
 }
 
-export function getCanvasAsImage(canvas, options = {}) {
+export async function getImageFromCanvas(canvas, options = {}) {
   const {type, quality} = options
 
-  const image = new Image()
+  const img = new Image()
 
   const dataURL = canvas.toDataURL(type, quality)
 
-  image.src = dataURL
+  img.src = dataURL
 
-  return image
+  await img.decode()
+
+  return img
 }
 
-export async function getMediaAsImage(media) {
-  const blob = await getMediaAsBlob(media)
+export async function getImageFromMedia(media) {
+  const blob = await getBlobFromMedia(media)
 
   const img = new Image()
   img.src = URL.createObjectURL(blob)
