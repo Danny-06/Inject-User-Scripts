0ccd965b46e90cd4b0f090894d5fbc32c64eeaf9
“Daniel” <daniel.fabian13x@gmail.com>
2022-12-13 01:11:09 +0100

    Update Generator

--- user-scripts/@libs/libs/generator-utils.js
+++ user-scripts/@libs/libs/generator-utils.js
@@ -2,21 +2,36 @@
  * 
  * @param {GeneratorFunction} generatorFunc 
  * @param  {...any} args 
+ * @returns {(...args: any[]) => Promise}
+ */
+export function getAsyncFuncFromGeneratorFunc(generatorFunc) {
+  return function asyncFunction(...args) {
+    const iterator = generatorFunc(...args)
+
+    return (function nextIteration(it, yieldArg) {
+      const {value: yieldValue, done} = it.next(yieldArg)
+  
+      if (done) {
+        return Promise.resolve(yieldValue)
+      }
+  
+      const isThenable = typeof yieldValue === 'object' && typeof yieldValue.then === 'function'
+      const promise = isThenable ? yieldValue : Promise.resolve(yieldValue)
+  
+      return promise.then(value => nextIteration(it, value), reason => it.throw(reason))
+    })(iterator)
+  }
+}
+
+
+/**
+ * 
+ * @param {GeneratorFunction} generatorFunc 
+ * @param  {...any} args 
  * @returns {Promise}
  */
-export function runGeneratorAsAsyncFunction(generatorFunc, ...args) {
-  const iterator = generatorFunc(...args)
+export function runGeneratorFuncAsAsyncFunc(generatorFunc, ...args) {
+  const asyncFunction = getAsyncFuncFromGeneratorFunc(generatorFunc)
 
-  return (function nextIteration(it, yieldArg) {
-    const {value: yieldValue, done} = it.next(yieldArg)
-
-    if (done) {
-      return Promise.resolve(yieldValue)
-    }
-
-    const isThenable = typeof yieldValue === 'object' && typeof yieldValue.then === 'function'
-    const promise = isThenable ? yieldValue : Promise.resolve(yieldValue)
-
-    return promise.then(value => nextIteration(it, value), reason => it.throw(reason))
-  })(iterator)
+  return asyncFunction(...args)
 }
