80a340c041cd15a3fde4dd32f14b64e1daa1507e
“Daniel” <64810647+Xion-14@users.noreply.github.com>
2023-01-04 13:38:01 +0100

    Try to fix yt poster

--- user-scripts/www.youtube.com/scripts/video-poster.mjs
+++ user-scripts/www.youtube.com/scripts/video-poster.mjs
@@ -1,8 +1,15 @@
 import { delay, waitForSelector } from '../../@libs/utils-injection.js'
 
-window.addEventListener('load', init)
-window.addEventListener('yt-navigate-start', init)
-window.addEventListener('yt-navigate-finish', init)
+window.addEventListener('load', catchInit)
+window.addEventListener('yt-navigate-start', catchInit)
+window.addEventListener('yt-navigate-finish', catchInit)
+
+const ytdApp = await waitForSelector('ytd-app')
+
+
+function catchInit() {
+  return init().catch(console.error)
+}
 
 async function init(event) {
   // await delay(500)
@@ -12,33 +19,7 @@ async function init(event) {
   const video = await waitForSelector('ytd-watch-flexy video')
   const videoID = new URL(location.href).searchParams.get('v')
 
-  const thumbnails = [
-    {
-      url: `https://i.ytimg.com/vi/${videoID}/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDiGpJmFn2jHznc6HEH-LRJf_9o0w`,
-      width: 168,
-      height: 94
-    },
-    {
-      url: `https://i.ytimg.com/vi/${videoID}/hqdefault.jpg?sqp=-oaymwEbCMQBEG5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLD9ri-LM_GTscnG7CyhFc2n_W9BKQ`,
-      width: 196,
-      height: 110
-    },
-    {
-      url: `https://i.ytimg.com/vi/${videoID}/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLATTHe9fL3ky8Gui8ZDZWhZdBXSzQ`,
-      width: 246,
-      height: 138
-    },
-    {
-      url: `https://i.ytimg.com/vi/${videoID}/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAw-732g01EauOmh8Xd3IPyT2Zw_w`,
-      width: 336,
-      height: 188
-    },
-    {
-      url: `https://i.ytimg.com/vi/${videoID}/maxresdefault.jpg`,
-      width: 1920,
-      height: 1080
-    }
-  ]
+  const thumbnails = ytdApp.data.playerResponse.videoDetails.thumbnail.thumbnails
 
   for (let i = thumbnails.length - 1; i >= 0; i--) {
     const url = thumbnails[i].url
