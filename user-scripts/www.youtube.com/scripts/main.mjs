83bb5e5c7f9615e2abfaff3f1a3ba6f2b1f770d6
“Daniel” <64810647+Xion-14@users.noreply.github.com>
2023-01-22 16:43:16 +0100

    Reformat

--- user-scripts/www.youtube.com/scripts/main.mjs
+++ user-scripts/www.youtube.com/scripts/main.mjs
@@ -2,7 +2,6 @@ import { handleSelectorLifeCycle, waitForSelector, delay } from '../../@libs/uti
 import { calidad1080pAutomatica, ContextMenuManager as ctxM } from './youtube-utils.js'
 import * as youtubeUtils from './youtube-utils.js'
 import * as ctxMenu  from './resource-data/context-menu-options.js'
-import { createWebComponent } from '../../@libs/libs/dom-utils.js'
 import _, { buildElement as $ } from '../../@libs/libs/functional-dom/index.js'
 import { addEventListener } from '../../@libs/libs/event-utils.js'
 
@@ -116,160 +115,156 @@ async function handleSecondaryInnerWatch() {
 
   }
 
-  class SecondaryTabs extends HTMLElement {
+  initSecondaryTabs()
 
-    _constructor_() {
-      const shadow = this.attachShadow({mode: 'open'})
+  function initSecondaryTabs() {
+    const shadow = secondaryInner.attachShadow({mode: 'open'})
 
-      const relatedBtn       = _.button({dataset: {id: 'related'}}, 'Related')
-      const playlistBtn      = _.button({dataset: {id: 'playlist'}}, 'Playlist')
-      const descriptionBtn   = _.button({dataset: {id: 'description'}}, 'Description')
-      const commentsBtn      = _.button({dataset: {id: 'comments'}}, 'Comments')
-      const chaptersBtn      = _.button({dataset: {id: 'chapters'}}, 'Chapters')
-      const autoChaptersBtn  = _.button({dataset: {id: 'auto-chapters'}}, 'Auto Chapters')
-      const transcriptionBtn = _.button({dataset: {id: 'transcription'}}, 'Transcription')
-      const liveChatBtn      = _.button({dataset: {id: 'live-chat'}}, 'Live Chat')
+    const relatedBtn       = _.button({dataset: {id: 'related'}}, 'Related')
+    const playlistBtn      = _.button({dataset: {id: 'playlist'}}, 'Playlist')
+    const descriptionBtn   = _.button({dataset: {id: 'description'}}, 'Description')
+    const commentsBtn      = _.button({dataset: {id: 'comments'}}, 'Comments')
+    const chaptersBtn      = _.button({dataset: {id: 'chapters'}}, 'Chapters')
+    const autoChaptersBtn  = _.button({dataset: {id: 'auto-chapters'}}, 'Auto Chapters')
+    const transcriptionBtn = _.button({dataset: {id: 'transcription'}}, 'Transcription')
+    const liveChatBtn      = _.button({dataset: {id: 'live-chat'}}, 'Live Chat')
 
-      if (new URLSearchParams(location.search).get('list') == null) {
-        panels.related.slot = 'active'
-        relatedBtn.classList.add('selected')
-      } else {
-        panels.playlist.slot = 'active'
-        playlistBtn.classList.add('selected')
-      }
-
-      playlistBtn?.addEventListener('click', event => {
-        const ytdWatchFlexy = document.querySelector('ytd-watch-flexy')
-
-        const playlistContainer = ytdWatchFlexy.querySelector('ytd-playlist-panel-renderer#playlist')
-
-        const playlistItemsContainer = playlistContainer.querySelector('.playlist-items')
-        const header = playlistContainer.querySelector('.header')
-
-        const selectedItem = playlistItemsContainer.querySelector(':scope > [selected]')
-
-        setTimeout(() => {
-          playlistItemsContainer.scrollTop = selectedItem.offsetTop - selectedItem.offsetHeight - header.offsetHeight - 50
-        })
-      })
-
-      const panelButtons = [
-        relatedBtn,
-        playlistBtn,
-        descriptionBtn,
-        commentsBtn,
-        chaptersBtn,
-        autoChaptersBtn,
-        transcriptionBtn,
-        liveChatBtn,
-      ]
-
-
-      const style = _.style({}, // css
-      `
-      .title {
-        font-size: 16px;
-      }
-
-      .panel-buttons {
-        flex-shrink: 0;
-
-        display: flex;
-        flex-wrap: wrap;
-        gap: 5px;
-
-        width: 100%;
-
-        padding-block: 1rem;
-      }
-
-      .panel-buttons > button {
-        all: unset;
-
-        flex-shrink: 0;
-        box-sizing: border-box;
-        font-size: 13px;
-        color: #eee;
-
-        padding-inline: 0.8em;
-        padding-block: 0.7em;
-
-        border-radius: 0.3em;
-
-        background-color: #222;
-
-        user-select: none;
-      }
-
-      .panel-buttons > button:is(:hover, :active) {
-        background-color: #444;
-      }
-
-      .panel-buttons > button:active {
-        background-color: #666;
-      }
-
-      .panel-buttons > button.selected {
-        background-color: #666;
-      }
-      `)
-
-      addEventListener(window, 'youtube-navigate', async event => {
-        await waitForSelector('ytd-watch-flexy #secondary #secondary-inner #panels')
-        await delay(500)
-
-        const oldPanels = secondaryInner.querySelectorAll(':scope > ytd-engagement-panel-section-list-renderer')
-
-        oldPanels.forEach(panel => panel.remove())
-
-        panelsContainer.after(...panelsContainer.children)
-
-        shadow.replaceChildren()
-
-        shadow.append(
-          style,
-  
-          _.div({class: 'title'}, 'Panels'),
-          _.div({class: 'panel-buttons'},
-            panelButtons.filter(button => panels[button.dataset.id] != null)
-          ),
-          _.slot({attributes: {name: 'active'}}),
-        )
-      }, {runImmediately: true})
-
-      function setActiveSlot(event) {
-        ;[...panels].forEach(panel => {
-          panel.removeAttribute('slot')
-
-          if (panel.localName === 'ytd-engagement-panel-section-list-renderer') {
-            // Show all of the panels
-            panel.setAttribute('visibility', 'ENGAGEMENT_PANEL_VISIBILITY_EXPANDED')
-
-            // Reset the `order` value for panels that has one explicitly
-            panel.style.order = '0'
-          }
-        })
-
-        panelButtons.forEach(button => {
-          button.classList.remove('selected')
-        })
-
-        const button = event.target
-
-        button.classList.add('selected')
-
-        panels[button.dataset.id].slot = 'active'
-      }
-
-      panelButtons.forEach(panel => {
-        panel.addEventListener('click', setActiveSlot)
-      })
-
+    if (new URLSearchParams(location.search).get('list') == null) {
+      panels.related.slot = 'active'
+      relatedBtn.classList.add('selected')
+    } else {
+      panels.playlist.slot = 'active'
+      playlistBtn.classList.add('selected')
     }
 
-  }
+    playlistBtn?.addEventListener('click', event => {
+      const ytdWatchFlexy = document.querySelector('ytd-watch-flexy')
 
-  createWebComponent(SecondaryTabs, secondaryInner)
+      const playlistContainer = ytdWatchFlexy.querySelector('ytd-playlist-panel-renderer#playlist')
+
+      const playlistItemsContainer = playlistContainer.querySelector('.playlist-items')
+      const header = playlistContainer.querySelector('.header')
+
+      const selectedItem = playlistItemsContainer.querySelector(':scope > [selected]')
+
+      setTimeout(() => {
+        playlistItemsContainer.scrollTop = selectedItem.offsetTop - selectedItem.offsetHeight - header.offsetHeight - 50
+      })
+    })
+
+    const panelButtons = [
+      relatedBtn,
+      playlistBtn,
+      descriptionBtn,
+      commentsBtn,
+      chaptersBtn,
+      autoChaptersBtn,
+      transcriptionBtn,
+      liveChatBtn,
+    ]
+
+
+    const style = _.style({}, // css
+    `
+    .title {
+      font-size: 16px;
+    }
+
+    .panel-buttons {
+      flex-shrink: 0;
+
+      display: flex;
+      flex-wrap: wrap;
+      gap: 5px;
+
+      width: 100%;
+
+      padding-block: 1rem;
+    }
+
+    .panel-buttons > button {
+      all: unset;
+
+      flex-shrink: 0;
+      box-sizing: border-box;
+      font-size: 13px;
+      color: #eee;
+
+      padding-inline: 0.8em;
+      padding-block: 0.7em;
+
+      border-radius: 0.3em;
+
+      background-color: #222;
+
+      user-select: none;
+    }
+
+    .panel-buttons > button:is(:hover, :active) {
+      background-color: #444;
+    }
+
+    .panel-buttons > button:active {
+      background-color: #666;
+    }
+
+    .panel-buttons > button.selected {
+      background-color: #666;
+    }
+    `)
+
+    addEventListener(window, 'youtube-navigate', async event => {
+      await waitForSelector('ytd-watch-flexy #secondary #secondary-inner #panels')
+      await delay(500)
+
+      const oldPanels = secondaryInner.querySelectorAll(':scope > ytd-engagement-panel-section-list-renderer')
+
+      oldPanels.forEach(panel => panel.remove())
+
+      panelsContainer.after(...panelsContainer.children)
+
+      shadow.replaceChildren()
+
+      shadow.append(
+        style,
+
+        _.div({class: 'title'}, 'Panels'),
+        _.div({class: 'panel-buttons'},
+          panelButtons.filter(button => panels[button.dataset.id] != null)
+        ),
+        _.slot({attributes: {name: 'active'}}),
+      )
+    }, {runImmediately: true})
+
+    function setActiveSlot(event) {
+      ;[...panels].forEach(panel => {
+        panel.removeAttribute('slot')
+
+        if (panel.localName === 'ytd-engagement-panel-section-list-renderer') {
+          // Show all of the panels
+          panel.setAttribute('visibility', 'ENGAGEMENT_PANEL_VISIBILITY_EXPANDED')
+
+          // Reset the `order` value for panels that has one explicitly
+          panel.style.order = '0'
+        }
+      })
+
+      panelButtons.forEach(button => {
+        button.classList.remove('selected')
+      })
+
+      const button = event.target
+
+      button.classList.add('selected')
+
+      panels[button.dataset.id].slot = 'active'
+    }
+
+    panelButtons.forEach(panel => {
+      panel.addEventListener('click', setActiveSlot)
+    })
+
+  }
 }
 
 
