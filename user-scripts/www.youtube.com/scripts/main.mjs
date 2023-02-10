4494ed7e90b6f51b86a640b1f957a12be0e61ca5
“Daniel” <64810647+Xion-14@users.noreply.github.com>
2023-02-10 14:46:37 +0100

    Add option to open banner channel uncropped

--- user-scripts/www.youtube.com/scripts/main.mjs
+++ user-scripts/www.youtube.com/scripts/main.mjs
@@ -30,6 +30,8 @@ setTimeout(() => {
 
 window.addEventListener('youtube-load', event => {
   handleSecondaryInnerWatch().catch(console.error)
+
+  openUncroppedBannerWithClick()
 })
 
 
@@ -442,4 +444,44 @@ async function initCustomContextMenu() {
   ])
 
   ctxMContainer.append(...ctxM.elementItems)
+}
+
+
+function openUncroppedBannerWithClick() {
+  window.addEventListener('click', event => {
+    const banner = (() => {
+      const target = event.target
+  
+      return target.matches('tp-yt-app-header-layout .banner-visible-area')
+        ? target
+        : null
+    })()
+  
+    if (banner == null) {
+      return
+    }
+  
+    const tabbedHeader = banner.closest('ytd-c4-tabbed-header-renderer')
+  
+    if (tabbedHeader == null) {
+      console.error('Parent banner not found', location.pathname)
+      return
+    }
+  
+    const bannerURL = (() => {
+      let url = tabbedHeader.style
+                  .getPropertyValue('--yt-channel-banner')
+                  .slice(
+                    'url('.length,
+                    - ')'.length
+                  )
+                  .replaceAll('\\', '')
+  
+      url = url.slice(0, url.indexOf('-fcrop'))
+  
+      return url
+    })()
+  
+    window.open(bannerURL)
+  })  
 }
