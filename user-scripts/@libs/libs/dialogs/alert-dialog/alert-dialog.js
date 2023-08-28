7a9bd1825643e2d537136a5aab5c008ecf0908c8
“Daniel” <64810647+Xion-14@users.noreply.github.com>
2023-08-28 16:12:46 +0200

    Update dialog to use Preact

--- user-scripts/@libs/libs/dialogs/alert-dialog/alert-dialog.js
+++ user-scripts/@libs/libs/dialogs/alert-dialog/alert-dialog.js
@@ -1,23 +1,61 @@
-import { getAllElementsMapWithBracketsId, importTemplateAndCSS } from "../../dom-utils.js"
+import { useRef } from '../../../preact/hooks.mjs'
+import html from '../../../preact/htm/html.mjs'
+import { render } from '../../../preact/preact.mjs'
+import { useSignal } from '../../../preact/signals.mjs'
+import DivShadow from '../../../preact/util-components/shadow-dom.js'
+import appendComponent from '../../../preact/util-functions/append-component.js'
+import useEffectOnce from '../../../preact/util-hooks/use-effect-once.js'
+import stylesheet from './alert-dialog.css' assert {type: 'css'}
 
 let isOpened = false
 
-let dialogStoreReference
-
 const resetOpenState = () => isOpened = false
 
-const [dialogTemplate, stylesheet] = await importTemplateAndCSS({
-  template: './alert-dialog.html',
-  stylesheet: './alert-dialog.css'
-}, import.meta.url)
 
+function Dialog(props) {
+  const { message, onAccept, onClose, ...attributes } = props
+
+  const isClosingSignal = useSignal(false)
+  const isClosing = isClosingSignal.value
+
+  const dialogOverlayRef = useRef(null)
+
+  useEffectOnce(() => {
+    const dialogOverlay = dialogOverlayRef.current
+
+    dialogOverlay.addEventListener('animationend', () => {
+      setTimeout(() => {
+        dialogOverlay.addEventListener('animationend', event => onClose(), {once: true})
+      })
+    }, {once: true})
+  })
+
+  return html`
+    <${DivShadow} ...${attributes} stylesheets=${[stylesheet]}>
+      <div ref=${dialogOverlayRef} class=${`dialog-menu-overlay ${isClosing ? 'closing' : ''}`}>
+          <div class=${`dialog-menu ${isClosing ? 'closing' : ''}`}>
+              <div class="message">${message}</div>
+              <div class="buttons">
+                  <button class="accept"
+                    onClick=${async () => {
+                      isClosingSignal.value = true
+
+                      onAccept()
+                    }}
+                  >Accept</button>
+              </div>
+          </div>
+      </div>
+    <//>
+  `
+}
 
 export function showAlertDialog(message = '(No message provided)') {
   // If the dialog is "opened" but the element is not in the DOM or
   // is not in the active document then reset the "opened" state
-  if (!dialogStoreReference || isOpened && (!dialogStoreReference.isConnected || dialogStoreReference.ownerDocument !== document)) {
-    resetOpenState()
-  }
+  // if (!dialogStoreReference || isOpened && (!dialogStoreReference.isConnected || dialogStoreReference.ownerDocument !== document)) {
+  //   resetOpenState()
+  // }
 
   if (isOpened) {
     return Promise.reject(new Error(`The dialog is still opened`))
@@ -25,33 +63,20 @@ export function showAlertDialog(message = '(No message provided)') {
 
   isOpened = true
 
-  const {firstElementChild: dialog} = dialogTemplate.clone(document)
-
-  dialog.shadowRoot.adoptedStyleSheets = [stylesheet]
-
-  const mapId = getAllElementsMapWithBracketsId(dialog, {shadowRoot: true})
-
-
-  const {dialogMenu, message: msgDialog, acceptBtn} = mapId
-
-  dialogStoreReference = dialogMenu
-
-
-  msgDialog.innerHTML = message
-
-  document.body.append(dialog)
-
   const promiseResult = new Promise(resolve => {
-    acceptBtn.addEventListener('click', event => resolve())
+    const dialogWrapper = appendComponent(
+      html`
+      <${Dialog} message=${message}
+        onAccept=${resolve}
+        onClose=${() => {
+          resetOpenState()
+          dialogWrapper.remove()
+        }}
+      >
+      </>`,
+      document.body
+    )
   })
 
-  return promiseResult.finally(() => {
-    dialog.classList.add('closing')
-    dialogMenu.classList.add('closing')
-
-    dialog.addEventListener('animationend', event => {
-      resetOpenState()
-      dialog.remove()
-    })
-  })
+  return promiseResult
 }
