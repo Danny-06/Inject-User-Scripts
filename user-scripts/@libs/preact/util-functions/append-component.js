84b09bedb13196d72f23f977b689483bf14cc83d
“Daniel” <64810647+Xion-14@users.noreply.github.com>
2023-08-29 00:05:06 +0200

    Update Custom Preact function

--- user-scripts/@libs/preact/util-functions/append-component.js
+++ user-scripts/@libs/preact/util-functions/append-component.js
@@ -1,11 +1,29 @@
+import { createPortal } from '../compat.mjs'
 import { render } from '../preact.mjs'
 
 export default function appendComponent(component, node) {
   const wrapper = document.createElement('div')
 
-  render(component, wrapper)
+  render(createPortal(component, node), wrapper)
 
   node.append(wrapper)
 
-  return wrapper
+  setTimeout(() => wrapper.remove())
+
+  return node
 }
+
+
+
+
+// import { render } from '../preact.mjs'
+
+// export default function appendComponent(component, node) {
+//   const wrapper = document.createElement('div')
+
+//   render(component, wrapper)
+
+//   node.append(wrapper)
+
+//   return wrapper
+// }
