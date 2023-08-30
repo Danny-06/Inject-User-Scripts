97a9e4e96468de795eca640b57420782aa9cd3bc
“Daniel” <64810647+Xion-14@users.noreply.github.com>
2023-08-30 22:38:21 +0200

    Improve custom scrollbar on glitch.com

---
+++ user-scripts/glitch.com/main.mjs
@@ -0,0 +1,9 @@
+import { getZoom } from '../../../@libs/utils-injection.js'
+
+const onVisualViewportResize = event => {
+  document.documentElement.style.setProperty('--visual-viewport-zoom-ratio', 1 / getZoom())
+}
+
+onVisualViewportResize()
+
+visualViewport.addEventListener('resize', onVisualViewportResize)
