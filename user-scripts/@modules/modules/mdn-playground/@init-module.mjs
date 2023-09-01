3d187981f4c6ee9f64c9e9301a05f953ba728640
“Daniel” <64810647+Xion-14@users.noreply.github.com>
2023-09-01 20:14:35 +0200

    MDN playground

--- user-scripts/@modules/modules/mdn-playground/@init-module.mjs
+++ user-scripts/@modules/modules/mdn-playground/@init-module.mjs
@@ -3,7 +3,10 @@ import { getModuleURL, injectCode } from '../../module-utils.js'
 const modulePath = getModuleURL(import.meta.url)
 
 injectCode(modulePath, {
- stylesheets: [
-   'main.css'
- ]
+  stylesheets: [
+    'main.css'
+  ],
+  scripts: [
+  'main.mjs'
+  ]
 })
