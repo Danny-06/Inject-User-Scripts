f8b9700c6026374ed738898e11dd06ae98448c5f
“Daniel” <64810647+Xion-14@users.noreply.github.com>
2023-09-06 18:07:23 +0200

    Add scroll to chess.com

--- user-scripts/@modules/domain-match-injection.mjs
+++ user-scripts/@modules/domain-match-injection.mjs
@@ -24,7 +24,8 @@ injectModulesWithDomainMatch({
   matchExpressions: [
     '*.google.*', 'netflix.com', 'clashroyale.com', '*.reddit.com',
     'stackoverflow.com', 'github.com', '*.facebook.com', '*.instagram.com', '*.w3schools.com',
-    'developer.mozilla.org', '*.crunchyroll.com', '*.kotlinlang.org', 'developer.android.com'
+    'developer.mozilla.org', '*.crunchyroll.com', '*.kotlinlang.org', 'developer.android.com',
+    'www.chess.com'
   ],
   modules: [
     'general-custom-scrollbar'
