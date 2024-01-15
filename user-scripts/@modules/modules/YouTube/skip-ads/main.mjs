847aa3844db655be0233db15f3f3c1a9183bf222
Daniel Fabián <daniel.fabian13x@gmail.com>
2024-01-16 00:41:06 +0100

    Improve video ad skip

--- user-scripts/@modules/modules/YouTube/skip-ads/main.mjs
+++ user-scripts/@modules/modules/YouTube/skip-ads/main.mjs
@@ -12,19 +12,37 @@ window.addEventListener('youtube-navigate', async event => {
 
   video = await waitForSelector('ytd-app ytd-watch-flexy video')
 
-  const playerParent = video.closest('.html5-video-player')
+  video.addEventListener('yt-video-ad-start', event => {
+    skipVideoAd(video)
+  })
 
-  video.addEventListener('loadeddata', async event => {
-    const isVideoAd = playerParent.classList.contains('ad-showing') || playerParent.classList.contains('ad-interrupting')
-
-    if (!isVideoAd) {
+  const dispatchYtVideoAdStart =  async event => {
+    if (!isVideoAd(video)) {
       return
     }
 
-    video.currentTime += video.duration
+    const ytVideoAdStartEvent = new CustomEvent('yt-video-ad-start')
+    video.dispatchEvent(ytVideoAdStartEvent)
+  }
 
-    const skipButton = await waitForSelector('.ytp-ad-skip-button-modern')
-    skipButton.click()
-  })
+  dispatchYtVideoAdStart()
 
+  video.addEventListener('loadeddata', dispatchYtVideoAdStart)
 })
+
+function isVideoAd(ytVideo) {
+  const ytPlayerParent = ytVideo.closest('.html5-video-player')
+
+  if (!ytPlayerParent) {
+    throw new Error(`Couldn't find player parent of the video`)
+  }
+
+  return ytPlayerParent.classList.contains('ad-showing') || ytPlayerParent.classList.contains('ad-interrupting')
+}
+
+async function skipVideoAd(ytVideo) {
+  ytVideo.currentTime += ytVideo.duration
+
+  const skipButton = await waitForSelector('.ytp-ad-skip-button-modern')
+  skipButton.click()
+}
